5877d5492e2d611dcadb1656f59d441a144f8c74
last-one <415079780@qq.com>
2017-11-18 00:27:35 +0800

    create the structure of G and D

---
+++ network.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# G(z)
+class generator(nn.Module):
+    '''
+        Generative Network
+    '''
+    def __init__(self, z_size=100, out_size=3, ngf=128):
+        super(generator, self).__init__()
+        self.z_size = z_size
+        self.ngf = ngf
+        self.out_size = out_size
+
+        self.main = nn.Sequential(
+            # input size is z_size
+            nn.ConvTranspose2d(self.z_size, self.ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(self.ngf * 8),
+            nn.ReLU(inplace=True)
+            # state size: (ngf * 8) x 4 x 4
+            nn.ConvTranspose2d(self.ngf * 8, self.ngf * 4, 4, 2, 1, bias=False)
+            nn.BatchNorm2d(self.ngf * 4)
+            nn.ReLU(inplace=True)
+            # state size: (ngf * 4) x 8 x 8
+            nn.ConvTranspose2d(self.ngf * 4, self.ngf * 2, 4, 2, 1, bias=False)
+            nn.BatchNorm2d(self.ngf * 2)
+            nn.ReLU(inplace=True)
+            # state size: (ngf * 2) x 16 x 16
+            nn.ConvTranspose2d(self.ngf * 2, self.ngf, 4, 2, 1)
+            nn.BatchNorm2d(self.ngf)
+            nn.ReLU(inplace=True)
+            # state size: ngf x 32 x 32
+            nn.ConvTranspose2d(self.ngf, self.out_size, 4, 2, 1, bias=False)
+            nn.Tanh()
+            # state size: 3 x 64 x 64
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(0.0, 0.02)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.normal_(1.0, 0.02)
+                m.bias.data.zero_()
+
+    def forward(self, input):
+
+        output = self.main(input)
+
+        return output
+
+# D
+class discriminator(nn.Module):
+    '''
+        Discriminative Network
+    '''
+    def __init__(self, in_size=3, ndf=128):
+        super(discriminator, self).__init__()
+        self.in_size = in_size
+        self.ndf = ndf
+
+        self.main = nn.Sequential(
+            # input size is in_size x 64 x 64
+            nn.Conv2d(self.in_size, self.ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: ndf x 32 x 32
+            nn.Conv2d(self.ndf, self.ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf * 2) x 16 x 16
+            nn.Conv2d(self.ndf * 2, self.ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf * 4) x 8 x 8
+            nn.Conv2d(self.ndf * 4, self.ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf * 8) x 4 x 4
+            nn.Conv2d(self.ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+            # state size: 1 x 1 x 1
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(0.0, 0.02)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.normal_(1.0, 0.02)
+                m.bias.data.zero_()
+
+    def forward(self, input):
+
+        output = self.main(input)
+
+        return output.view(-1, 1).squeeze(1)
